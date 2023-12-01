efa7b3faab6ca2aed6ea821d8f6b54d828cf4293
Alexander Ernst <Alexander.Ernst@duagon.com>
2023-12-01 15:50:44 +0100

    Add project files.

---
+++ AdventOfCode2022/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace AdventOfCode2022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n\t --- Code of Advent 2022 --- ");
+            var p1 = new Program();
+            p1.Day01();
+            p1.Day01Alternate();
+            Console.ReadKey();
+        }
+
+        private void Day01()
+        {
+            var text = File.ReadAllText("..\\..\\..\\input.txt");
+            var splitSections = text.Split("\n\n");
+            var calories = new List<int>();
+
+            foreach (var section in splitSections)
+            {
+                var currentCalorie = 0;
+                var individualItem = section.Split("\n");
+
+                foreach (var item in individualItem)
+                {
+                    if (!string.IsNullOrEmpty(item))
+                    {
+                        currentCalorie += int.Parse(item);
+                    }
+                }
+
+                calories.Add(currentCalorie);
+            }
+
+            var top3Sum = calories.OrderDescending().Take(3).Sum();
+            Console.WriteLine("\nDay 01 - Part 1 and 2 - Solution 1:");
+            Console.WriteLine("Top calorie count: " + calories.Max());
+            Console.WriteLine("Top 3 calories sum: " + top3Sum);
+        }
+
+        private void Day01Alternate()
+        {
+            var lines = File.ReadAllLines("..\\..\\..\\input.txt");
+            var currentCalorie = 0;
+            var calories = new List<int>();
+
+            foreach (var line in lines)
+            {
+                if (string.IsNullOrEmpty(line)) // we know new section starts here
+                {
+                    calories.Add(currentCalorie);
+                    currentCalorie = 0;
+                }
+                else
+                {
+                    currentCalorie += int.Parse(line);
+                }
+            }
+
+            var top3Sum = calories.OrderDescending().Take(3).Sum();
+            Console.WriteLine("\nDay 01 - Part 1 and 2 - Solution 2:");
+            Console.WriteLine("Top calorie count: " + calories.Max());
+            Console.WriteLine("Top 3 calories sum: " + top3Sum);
+        }
+    }
+}
---
+++ AdventOfCode2023/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace AdventOfCode2023
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("\n\t --- Code of Advent 2023 --- ");
+            var p1 = new Program();
+            p1.Day01(false);
+            p1.Day01(true);
+            Console.ReadKey();
+        }
+
+        private void Day01(bool part2)
+        {
+            var lines = File.ReadAllLines("..\\..\\..\\input.txt");
+            var candidates = new List<(string key, int value)> { ("1", 1), ("2", 2), ("3", 3), ("4", 4), ("5", 5), ("6", 6), ("7", 7), ("8", 8), ("9", 9), ("0", 0), };
+            if (part2)
+            {
+                candidates.AddRange(new List<(string, int)> { ("one", 1), ("two", 2), ("three", 3), ("four", 4), ("five", 5), ("six", 6), ("seven", 7), ("eight", 8), ("nine", 9) });
+            }
+
+            var nums = new List<int>();
+
+            foreach (var line in lines)
+            {
+                // this gets every number, into an array/collection, and index is -1 when not found and index of place found when found
+                // then we do a where to only get ones where it is found
+                // then we get first and last
+
+                int firstDigit = candidates.Select(candidate => new { Index = line.IndexOf(candidate.key), Value = candidate.value }).Where(x => x.Index >= 0).OrderBy(x => x.Index).First().Value;
+                int lastDigit = candidates.Select(candidate => new { Index = line.LastIndexOf(candidate.key), Value = candidate.value }).Where(x => x.Index >= 0).OrderBy(x => x.Index).Last().Value;
+                var finalNumber = int.Parse(firstDigit.ToString() + lastDigit.ToString());
+                // Console.WriteLine("Line: " + line + "\n\t First Digit: " + firstDigit + "\t Last Digit: " + lastDigit);
+                nums.Add(finalNumber);
+            }
+
+            var sum = nums.Sum();
+
+            Console.WriteLine("\nDay 01 - " + (part2 ? "Part 2:" : "Part 1:"));
+            Console.WriteLine("Sum of digits: " + sum);
+        }
+    }
+}
